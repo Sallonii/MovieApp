2a207c632e37d283829464cd2e9d7f571faad5aa
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-05-10 22:33:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css'
 import LoginForm from './components/LoginForm'
 import PopularPage from './components/PopularPage'
 import Home from './components/Home'
+import MovieDetails from './components/MovieDetails'
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/login" component={LoginForm} />
     <Route exact path="/popular" component={PopularPage} />
+    <Route exact path="/movies/:id" component={MovieDetails} />
   </Switch>
 )
 
