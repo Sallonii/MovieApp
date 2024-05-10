8facd20b631201e2d37451d5d38e2faa275bb0a6
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-05-10 22:37:11 +0530

    Update index.js

--- src/components/ReactSlick/index.js
+++ src/components/ReactSlick/index.js
@@ -83,7 +83,7 @@ class ReactSlick extends Component {
   }
 
   renderLoader = () => (
-    <div className="loader-container" testid="loader">
+    <div className="slick-loader-container" testid="loader">
       <Loader type="TailSpin" color="#D81F26" height={50} width={50} />
     </div>
   )
