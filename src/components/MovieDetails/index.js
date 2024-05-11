e4bb57a4b214599d62ef481d077e084bb5a0b045
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-05-11 08:13:09 +0530

    Update index.js

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,6 +1,9 @@
 import {Component} from 'react'
 
 import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+
+import Header from '../Header'
 
 import './index.css'
 
@@ -12,7 +15,7 @@ const apiConstants = {
 }
 
 class MovieDetails extends Component {
-  state = {movieDetails: {}}
+  state = {movieDetails: {}, movieStatus: apiConstants.initial}
 
   componentDidMount() {
     this.getMovieDetails()
@@ -50,10 +53,14 @@ class MovieDetails extends Component {
       similarMovies: updatedSimilarMovies,
       languages: updatedLanguages,
     }
-    this.setState({movieDetails: updatedMovieDetails})
+    this.setState({
+      movieDetails: updatedMovieDetails,
+      movieStatus: apiConstants.success,
+    })
   }
 
   getMovieDetails = async () => {
+    this.setState({movieStatus: apiConstants.inProgress})
     const {match} = this.props
     const {params} = match
     const {id} = params
@@ -75,8 +82,109 @@ class MovieDetails extends Component {
     }
   }
 
+  renderLoader = () => (
+    <div className="movie-details-loader-container" testid="loader">
+      <Loader type="TailSpin" color="#D81F26" height={50} width={50} />
+    </div>
+  )
+
+  renderMovieDetails = () => {
+    const {movieDetails} = this.state
+    const {
+      adult,
+      overview,
+      releaseDate,
+      title,
+      runtime,
+      posterPath,
+    } = movieDetails
+    const year = releaseDate.split('-')[0]
+    const runtimeHours = Math.round(runtime / 60)
+    const runtimeMinutes = runtime % 60
+    return (
+      <>
+        <div
+          className="movie-container"
+          style={{backgroundImage: `url('${posterPath}')`}}
+        >
+          <div className="content-container">
+            <h1>{title}</h1>
+            <div className="date-year">
+              <p className="date-item">{`${runtimeHours}h ${runtimeMinutes}m`}</p>
+              {!adult && <p className="date-item ua">U/A</p>}
+              <p className="date-item">{year}</p>
+            </div>
+            <p>{overview}</p>
+            <button type="button" className="play-btn">
+              Play
+            </button>
+          </div>
+        </div>
+        {this.renderMovieDetailsBottomPage()}
+      </>
+    )
+  }
+
+  renderMovieDetailsBottomPage = () => {
+    const {movieDetails} = this.state
+    const {genres} = movieDetails
+    return (
+      <div className="movie-details-bottom-container">
+        <ul className="movie-details-ul-container">
+          <li>
+            <h1 className="list-content-heading">Genres</h1>
+            <ul className="genre-ul-cont">
+              {genres.map(eachGenre => (
+                <li key={eachGenre.id}>
+                  <p className="list-content-item">{eachGenre.name}</p>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li>
+            <h1 className="list-content-heading">Audio Available</h1>
+            <p className="list-content-item">Action</p>
+            <p className="list-content-item">Superhero</p>
+            <p className="list-content-item">Fantasy</p>
+            <p className="list-content-item">adventure</p>
+          </li>
+          <li>
+            <h1 className="list-content-heading">Rating Count</h1>
+            <p className="list-content-item">Action</p>
+            <h1 className="list-content-heading">Rating Average</h1>
+            <p className="list-content-item">Fantasy</p>
+          </li>
+          <li>
+            <h1 className="list-content-heading">Budget</h1>
+            <p className="list-content-item">Action</p>
+            <h1 className="list-content-heading">Release Date</h1>
+            <p className="list-content-item">Fantasy</p>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
+  renderMovies = () => {
+    const {movieStatus} = this.state
+
+    switch (movieStatus) {
+      case apiConstants.inProgress:
+        return this.renderLoader()
+      case apiConstants.success:
+        return this.renderMovieDetails()
+      default:
+        return null
+    }
+  }
+
   render() {
-    return <h1>Hi</h1>
+    return (
+      <div className="movie-main-container">
+        <Header />
+        {this.renderMovies()}
+      </div>
+    )
   }
 }
 
