bf7bc4e93a2e1cf45c1a071c037a0dd4d3d13749
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-06-05 17:35:17 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -5,8 +5,10 @@ import {Link} from 'react-router-dom'
 const NotFound = () => (
   <div className="not-found-container">
     <h1>Lost Your Way?</h1>
-    <p>we are sorry, the page you requested could not be found</p>
-    <p>Please go back to the homepage</p>
+    <p>
+      we are sorry, the page you requested could not be found Please go back to
+      the homepage.
+    </p>
     <Link to="/" className="nav-item">
       <button type="button" className="go-home-btn">
         Go to Home
